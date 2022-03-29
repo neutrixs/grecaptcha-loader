135d3fc4869e58e99625a13a525ccbe0afb0228e
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-29 17:51:51 +0700

    declare module

--- index.d.ts
+++ index.d.ts
@@ -8,3 +8,10 @@ type setOptionsFunction = (optionsToSet: reCAPTCHAAPIParam) => void
 type loadFunction = () => Promise<void>
 type waitScriptLoadFunction = (element: HTMLScriptElement) => Promise<void>
 type waitGrecaptchaFunction = () => Promise<void>
+
+declare module 'grecaptcha-loader' {
+    const load: loadFunction
+    const setOptions: setOptionsFunction
+
+    export { load, setOptions }
+}
